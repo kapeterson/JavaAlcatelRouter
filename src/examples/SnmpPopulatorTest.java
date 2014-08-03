38512bdbda25f93a66c7271e0ea16ee4eb9acd7a
kp109p <kp109p@TXCDTL09KP109P.ITServices.sbc.com>
2014-08-03 10:45:55 -0500

    [fixed]Catch exception when populating router

--- src/examples/SnmpPopulatorTest.java
+++ src/examples/SnmpPopulatorTest.java
@@ -18,23 +18,26 @@ public class SnmpPopulatorTest {
 		String comm = args[1];
 		
 		System.out.println("IP = " + ip);
-		SRSnmpPopulator populator = new SRSnmpPopulator(ip, comm);
-		populator.populateHardware();
+		try {
+			SRSnmpPopulator populator = new SRSnmpPopulator(ip, comm);
+			populator.populateHardware();
 		
-		SRChassisObject router = populator.getRouter();
+			SRChassisObject router = populator.getRouter();
 		
-		System.out.println("System name is " + router.System.getHostName());
+			System.out.println("System name is " + router.System.getHostName());
 		
-		System.out.format("%-8s %-20s %-15s %-15s %-15s %n", "CARD", "TYPE", "SN", "PN", "MD");
-		System.out.format("%-8s %-20s %-15s %-15s %-15s %n", "--------", "---------------", "---------------", "---------------", "---------------");
+			System.out.format("%-8s %-20s %-15s %-15s %-15s %n", "CARD", "TYPE", "SN", "PN", "MD");
+			System.out.format("%-8s %-20s %-15s %-15s %-15s %n", "--------", "---------------", "---------------", "---------------", "---------------");
 
-		for ( Integer key : router.Cards.getCards().keySet()){
+			for ( Integer key : router.Cards.getCards().keySet()){
 			
-			SRCardObject card = router.Cards.getCard(key);
-			System.out.format("%-8d %-20s %-15s %-15s %-15s%n", card.getSlotNumber(), card.getCardType().trim(), card.getSerialNumber().trim(), card.getPartNumber().trim(), card.getManufactureDate().trim());
+				SRCardObject card = router.Cards.getCard(key);
+				System.out.format("%-8d %-20s %-15s %-15s %-15s%n", card.getSlotNumber(), card.getCardType().trim(), card.getSerialNumber().trim(), card.getPartNumber().trim(), card.getManufactureDate().trim());
+			}
+		
+		} catch ( Exception e) {
+			System.out.println("We caught an exception");
 		}
-		
-		
 	}
 	
 }
