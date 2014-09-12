f8f642d826718bc5398cb2540c620bed635f116f
kp109p <kp109p@att.com>
2014-09-12 12:08:59 -0700

    added comments and javadoc

--- src/parser/alu/config/sr7x50/card/CardParser.java
+++ src/parser/alu/config/sr7x50/card/CardParser.java
@@ -10,24 +10,45 @@ import parser.CommandHandler;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
+/**
+ * CardParser handles the main functionality for parsing cards int eh card configuration section <br>
+ * When done, the router will have all its card populated
+ * 
+ * @author Kris Peterson
+ *
+ */
 public class CardParser extends ConfigurationSection{
 
+	/**
+	 * Constructor for Card Context parser, 
+	 * @param router	SRChassisObject that will be populated
+	 * @param contextChangeHandler	ContextChange handler to callback when when chanting context
+	 */
 	public CardParser(SRChassisObject router, ContextChange contextChangeHandler){
 		super("CONFIG.CARD", router, contextChangeHandler);
 		this.commandHash.put(Pattern.compile("^card ([0-9]+)"), new CommandHandler("setCardContext",false));
 	}
 	
+
+	/**
+	 * Change to card context
+	 * @param matcher Matcher that contains the card number in the match
+	 */
 	public void setCardContext(Matcher matcher){
-		//System.out.println("Matched card " + matcher.group(1));
-		//this.setSectionDepth(this.lastDepth);
+
 		int cnum = Integer.parseInt(matcher.group(1));
 		ConfigurationSection iomSection = new IOMParser(this.router, this.contextChange, cnum);
 		iomSection.setParent(this);
 		iomSection.setSectionDepth(this.lastDepth);
 		this.getContextNotifier().contextChangeCallback(this, iomSection);
+		
 	}
 	
+	
+	/**
+	 * Use default handler for exiting section
+	 * {@link ConfigurationSection:#exitSection(Matcher)}
+	 */
 	public void exitSection(Matcher matcher){
 		
 		super.defaultExitHandler(matcher);
--- src/parser/alu/config/sr7x50/card/IOMParser.java
+++ src/parser/alu/config/sr7x50/card/IOMParser.java
@@ -9,9 +9,22 @@ import parser.ConfigurationSection;
 import router.alcatel.router.SRChassisObject;
 import router.alcatel.router.card.*;
 
+/**
+ * IOM Parser for parsing all configuration inside each individual card context
+ * @author pete
+ *
+ */
 public class IOMParser extends ConfigurationSection {
 	
+	/** Model of the SRCardObject **/
 	SRCardObject card = null;
+	
+	/**
+	 * Instantiates the IOM Parser for a single card
+	 * @param router	SRChassisObject to be populated
+	 * @param contextChangeHandler	handler to be notified on context change
+	 * @param cardNumber	Card number to populate
+	 */
 	public IOMParser(SRChassisObject router, ContextChange contextChangeHandler, int cardNumber){
 		
 		super("CONFIG.CARD.IOM", router, contextChangeHandler);
@@ -19,11 +32,19 @@ public class IOMParser extends ConfigurationSection {
 		this.commandHash.put(Pattern.compile("^card\\-type (.*)"), new CommandHandler("setCardType",false));
 	}
 	
+	/**
+	 * Sets the card type
+	 * @param matcher	Matcher containing the card type in group 1
+	 */
 	public void setCardType(Matcher matcher){
 		//System.out.println("Card-type = " + matcher.group(1));
 		this.card.setCardType(matcher.group(1));
 	}
 	
+	/**
+	 * Custom exitSection handler that adds the parsed card to the router object.
+	 * @param	matcher	Matcher that contains exit but is not needed
+	 */
 	public void exitSection(Matcher matcher){
 		if ( this.getLastCommandDepth() == this.sectionDepth) {
 			//System.out.format("Exiting card %s of card type %s\n", card.getSlotNumber(), card.getCardType() );
