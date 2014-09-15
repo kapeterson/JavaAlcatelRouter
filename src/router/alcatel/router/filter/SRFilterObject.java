9281a9aee22e8e8830e9c7ac94b870eff31a39af
kp109p <kp109p@att.com>
2014-09-15 07:34:17 -0700

    Created filter object

---
+++ src/router/alcatel/router/filter/SRFilterObject.java
@@ -0,0 +1,20 @@
+package router.alcatel.router.filter;
+
+public abstract class SRFilterObject {
+	
+	protected Integer filterNumber = 0;
+	protected String description = "";
+	
+	public SRFilterObject(){
+		this.filterNumber = 0;
+		this.description = "";
+	}
+	public SRFilterObject(Integer filterNumber){
+		this.filterNumber = filterNumber;
+		this.description = "";
+	}
+	
+	public void setFilterNumber(Integer number){
+		this.filterNumber = number;
+	}
+}
