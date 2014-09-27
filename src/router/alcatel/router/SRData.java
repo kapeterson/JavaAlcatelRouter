ba414bec1af7a04c441057aa94f72bb71b859edb
kp109p <kp109p@TXCDTL09KP109P.ITServices.sbc.com>
2014-09-26 21:26:35 -0500

    added static class for different data for valid types etc

---
+++ src/router/alcatel/router/SRData.java
@@ -0,0 +1,17 @@
+package router.alcatel.router;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SRData {
+
+	//public static ArrayList<String> PORT_ENET_ENCAP = new ArrayList<String>();
+	public final static List<String> PORT_ENCAP_TYPES = new ArrayList<String>();
+
+	static {
+		PORT_ENCAP_TYPES.add("null");
+		PORT_ENCAP_TYPES.add("dot1q");
+		PORT_ENCAP_TYPES.add("qinq");
+	}
+	
+}
--- src/router/alcatel/router/port/SRPortEthernet.java
+++ src/router/alcatel/router/port/SRPortEthernet.java
@@ -5,25 +5,27 @@ import java.util.List;
 
 import router.alcatel.router.AlcatelObject;
 import router.alcatel.router.AlcatelObjectType;
+import router.alcatel.router.SRData;
 
 public class SRPortEthernet extends AlcatelObject {
  
-	protected List<String> encapTypes = new ArrayList<String>(); 
+	//protected List<String> encapTypes = new ArrayList<String>(); 
 	
 	protected String encapsulation = "null";
 	public SREthernetNetwork NETWORK = new SREthernetNetwork();
 	
 	public SRPortEthernet(){
 		super(AlcatelObjectType.PORTETHERNET);
-		encapTypes.add("null");
-		encapTypes.add("dot1q");
-		encapTypes.add("qinq");
+		//encapTypes.add("null");
+		//encapTypes.add("dot1q");
+		//encapTypes.add("qinq");
 
 	}
 
 	
 	public void setEncapType(String encapType){
-		if ( encapTypes.contains(encapType)){
+		
+		if ( SRData.PORT_ENCAP_TYPES.contains(encapType)){
 			this.encapsulation = encapType;
 		} else {
 			System.out.println("ERROR: Invalid encap type " + encapType + " when setting port");
