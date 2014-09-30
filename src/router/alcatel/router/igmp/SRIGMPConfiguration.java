b24ae4aebac024b2bb9337e2a165fc1f777fa355
kp109p <kp109p@TXCDTL09KP109P.ITServices.sbc.com>
2014-09-30 15:56:10 -0500

    added some IGMP documentation

--- src/router/alcatel/router/igmp/SRIGMPConfiguration.java
+++ src/router/alcatel/router/igmp/SRIGMPConfiguration.java
@@ -3,22 +3,36 @@ import java.util.TreeMap;
 
 import router.alcatel.router.pim.SRPIMInterface;
 
+
+/**
+ * Models IGMP Configuration on 7x50
+ * @author Kris Peterson
+ *
+ */
 public class SRIGMPConfiguration {
 	
+	/** TreeMap of IGMP Interfaces.  Key=name value=SRIGMPInterface **/
 	protected TreeMap<String, SRIGMPInterface> interfaces;
 	
 	public SRIGMPConfiguration(){
 		this.interfaces = new TreeMap<String, SRIGMPInterface>();
 	}
 	
+	
+	/** Adds the IGMPInterface to the configuration **/
 	public void addInterface(SRIGMPInterface igmpInterface){
+		
 		this.interfaces.put(igmpInterface.getName(), igmpInterface);
 	}
 	
+	
+	/** Get the IGMPInterface with the given name **/
 	public SRIGMPInterface getInterface(String ifaceName){
 		return this.interfaces.get(ifaceName);
 	}
 	
+	
+	/** Get a TreeMap of all the IGMPInterfaces **/
 	public TreeMap<String, SRIGMPInterface> getInterfaces(){
 		return this.interfaces;
 		
--- src/router/alcatel/router/igmp/SRIGMPInterface.java
+++ src/router/alcatel/router/igmp/SRIGMPInterface.java
@@ -3,6 +3,11 @@ package router.alcatel.router.igmp;
 import router.alcatel.router.AlcatelObject;
 import router.alcatel.router.AlcatelObjectType;
 
+/**
+ * Models an IGMP Interface Configuration
+ * @author Kris Peterson
+ *
+ */
 public class SRIGMPInterface extends AlcatelObject{
 	
 	public SRIGMPInterface(String interfacename){
