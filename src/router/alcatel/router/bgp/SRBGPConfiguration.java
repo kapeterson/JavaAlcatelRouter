163649fc343b467f316da208d8f5947a30f92ff7
kp109p <kp109p@TXCDTL09KP109P.ITServices.sbc.com>
2014-09-30 15:18:34 -0500

    added some bgp documentation

--- src/router/alcatel/router/bgp/SRBGPConfiguration.java
+++ src/router/alcatel/router/bgp/SRBGPConfiguration.java
@@ -2,27 +2,55 @@ package router.alcatel.router.bgp;
 
 import java.util.TreeMap;
 
+/**
+ * Models BGP configuration section on the 7x50 Router.
+ * @author Kris Peterson
+ *
+ */
 public class SRBGPConfiguration {
 
+	/** list of BGP groups configured **/
 	protected TreeMap<String, SRBGPGroup> groups = new TreeMap<String, SRBGPGroup>();
 	
+	/** Constructor **/
 	public SRBGPConfiguration(){
 		
 	}
 	
+	
+	/**
+	 * Adds a bgp group to the configuration
+	 * @param group SRBGPGroup object to add
+	 */
 	public void addBGPGroup(SRBGPGroup group){
 		this.groups.put(group.getName(), group);
 	}
 	
+	/**
+	 * Is a group with the provided name configured?
+	 * @param groupName
+	 * @return boolean
+	 */
 	public boolean hasGroup(String groupName){
 		return this.groups.containsKey(groupName);
 	}
 	
+	
+	/**
+	 * Get a TreeMap list of all the bgp groups.
+	 * Group name is the key and the SRBGPGroup is the value
+	 * @return
+	 */
 	public TreeMap<String, SRBGPGroup>  getGroups(){
 		return this.groups;
 		
 	}
 	
+	/**
+	 * Get the SRBGP group with the provided name
+	 * @param groupName
+	 * @return the SRBGPGroup
+	 */
 	public SRBGPGroup getGroup(String groupName){
 		return this.groups.get(groupName);
 	}
--- src/router/alcatel/router/bgp/SRBGPGroup.java
+++ src/router/alcatel/router/bgp/SRBGPGroup.java
@@ -6,22 +6,42 @@ import router.alcatel.router.*;
 
 public class SRBGPGroup extends AlcatelObject {
 	
+	/** List of the ip address represented as a string of all neighbors **/
 	protected ArrayList<String> neighbors = new ArrayList<String>();
 
 	
+	/**
+	 * Create a BGP group with the provided groupName
+	 * @param groupName 
+	 */
 	public SRBGPGroup(String groupName){
 		super(AlcatelObjectType.BGPGROUP);
 		this.setName(groupName);
 	}
 	
+	
+	/**
+	 * Adds a neighbor with the ip address provided in string format
+	 * @param neighborIP
+	 */
 	public void addNeighbor(String neighborIP){
 		this.neighbors.add(neighborIP);
 	}
 	
+	
+	/**
+	 * Does the bgp group have the neighbor configured
+	 * @param neighborIP
+	 * @return
+	 */
 	public boolean hasNeighbor(String neighborIP){
 		return this.neighbors.contains(neighborIP);
 	}
 	
+	/**
+	 * Get an ArrayList containing all the neighbors int he group
+	 * @return ArrayList of string containing the ip of all neighbors
+	 */
 	public ArrayList<String> getNeighbors(){
 		return this.neighbors;
 	}
