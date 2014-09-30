a940dadb95d334be279a1eb46fd1f52d95c54f43
kp109p <kp109p@TXCDTL09KP109P.ITServices.sbc.com>
2014-09-30 15:56:49 -0500

    added some impm documentation

--- src/router/alcatel/router/impm/SRBandwidthPolicy.java
+++ src/router/alcatel/router/impm/SRBandwidthPolicy.java
@@ -3,6 +3,12 @@ package router.alcatel.router.impm;
 import router.alcatel.router.AlcatelObject;
 import router.alcatel.router.AlcatelObjectType;
 
+
+/**
+ * Models an IMPM Bandwidth policy
+ * @author kp109p
+ *
+ */
 public class SRBandwidthPolicy extends AlcatelObject {
 	
 	public SRBandwidthPolicy(String policyName){
