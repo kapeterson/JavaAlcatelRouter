a187448315ba50c310e6f2308eda018700d878b8
kapeterson <kp109p@att.com>
2015-01-30 11:40:14 -0600

    started implementting sap-egress details for fc

---
+++ src/router/alcatel/router/qos/ForwardingClass.java
@@ -0,0 +1,15 @@
+package router.alcatel.router.qos;
+
+public class ForwardingClass {
+	
+	int queue = 0;
+	
+	public void setQueue(int q){
+		this.queue = q;
+	}
+	
+	public int getQueue(){
+		return this.queue;
+		
+	}
+}
--- src/router/alcatel/router/qos/SAPEgressQOSPolicy.java
+++ src/router/alcatel/router/qos/SAPEgressQOSPolicy.java
@@ -1,5 +1,7 @@
 package router.alcatel.router.qos;
 
+import java.util.TreeMap;
+
 import router.alcatel.router.AlcatelObjectType;
 
 /**
@@ -8,9 +10,28 @@ import router.alcatel.router.AlcatelObjectType;
  *
  */
 public class SAPEgressQOSPolicy extends SAPQOSPolicy {
+	String description = "";
+	
+	TreeMap<String, ForwardingClass> fc =  new TreeMap<String, ForwardingClass>();
 	
 	public SAPEgressQOSPolicy(){
 		super(AlcatelObjectType.SAPEGRESSQOSPOLICY);
 		
+	}
+	
+	public void addFC(String fcname){
+		
+		fc.put(fcname, new ForwardingClass());
+		
+	}
+	
+	
+	public boolean setForwardingClassQueue(String fcname, int queue){
+		fc.get(fcname).setQueue(queue);
+		return true;
+	}
+	
+	public TreeMap<String, ForwardingClass> getForwardingClasses(){
+		return this.fc;
 	}
 }
